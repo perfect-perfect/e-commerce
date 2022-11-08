bd4873204dda1437923ff5bf3b9d8ab7b46e4f57
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-11-08 16:39:33 -0500

    began nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <header>
+            <nav className='nav'>
+                <ul>
+                    <li>MENSWEAR</li>
+                    <li>WOMENSWEAR</li>
+                    <li>EVERYTHING ELSE</li>
+                    <li>SEARCH</li>
+                </ul>
+                <div>
+                    <h1>STORE</h1>
+                </div>
+                <ul>
+                    <li>ENGLISH</li>
+                    <li>ACCOUNT</li>
+                    <li>WISHLIST</li>
+                    <li>Shopping Bag</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Nav
