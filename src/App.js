eba4290c63d6db0a88b72b7a52ef692117930133
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-11-09 15:10:32 -0500

    rough draft of Nav.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 
 function App() {
+
+  const [categories] = useState([
+    {
+      name: 'MENSWEAR',
+    },
+    {
+      name: 'WOMENSWEAR'
+    },
+    {
+      name: 'EVERYTHING ELSE'
+    },
+    {
+      name: 'SEARCH'
+    },
+    {
+      name: 'ENGLISH'
+    },
+    {
+      name: 'ACCOUNT'
+    },
+    {
+      name: 'WISHLIST'
+    },
+    {
+      name: 'SHOPPING BAG'
+    }
+  ])
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
   return (
     <div>
-      <Nav></Nav>
+      <Nav 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,123 @@
-import React from 'react'
+import React from "react";
 
-function Nav() {
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+
   return (
     <div>
-        <header>
-            <nav className='nav'>
-                <ul>
-                    <li>MENSWEAR</li>
-                    <li>WOMENSWEAR</li>
-                    <li>EVERYTHING ELSE</li>
-                    <li>SEARCH</li>
-                </ul>
-                <div>
-                    <h1>STORE</h1>
-                </div>
-                <ul>
-                    <li>ENGLISH</li>
-                    <li>ACCOUNT</li>
-                    <li>WISHLIST</li>
-                    <li>Shopping Bag</li>
-                </ul>
-            </nav>
-        </header>
+      <header>
+        <nav className="nav">
+          <ul>
+            <li
+              className={`${
+                currentCategory.name === `MENSWEAR` && `navActive`
+              }`}
+              key={`MENSWEAR`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('MENSWEAR');
+                }}
+              >
+                MENSWEAR
+              </span>
+            </li>
+            <li
+              className={`${
+                currentCategory.name === `WOMENSWEAR` && `navActive`
+              }`}
+              key={`WOMENSWEAR`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('WOMENSWEAR');
+                }}
+              >
+                WOMENSWEAR
+              </span>
+            </li>
+            <li
+              className={`${
+                currentCategory.name === `EVERYTHING ELSE` && `navActive`
+              }`}
+              key={`EVERYTHING ELSE`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('EVERYTHING ELSE');
+                }}
+              >
+                EVERYTHING ELSE
+              </span>
+            </li>
+            <li
+              className={`${currentCategory.name === `SEARCH` && `navActive`}`}
+              key={`SEARCH`}
+            >
+              SEARCH
+            </li>
+          </ul>
+          <div>
+            <h1>STORE</h1>
+          </div>
+          <ul>
+            <li
+              className={`${currentCategory.name === `ENGLISH` && `navActive`}`}
+              key={`ENGLISH`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('ENGLISH');
+                }}
+              >
+                ENGLISH
+              </span>
+            </li>
+            <li
+              className={`${currentCategory.name === `ACCOUNT` && `navActive`}`}
+              key={`ACCOUNT`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('ACCOUNT');
+                }}
+              >
+                ACCOUNT
+              </span>
+            </li>
+            <li
+              className={`${
+                currentCategory.name === `WISHLIST` && `navActive`
+              }`}
+              key={`WISHLIST`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('WISHLIST');
+                }}
+              >
+                WISHLIST
+              </span>
+            </li>
+            <li
+              className={`${
+                currentCategory.name === `SHOPPING BAG` && `navActive`
+              }`}
+              key={`SHOPPING BAG`}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory('SHOPPING BAG');
+                }}
+              >
+                SHOPPING BAG
+              </span>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
