15f9c61da1a2d7e14cd66c9d8c285eb94be121d2
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-11-12 00:55:08 -0500

    resized images in Gallery

--- src/components/Gallery/style.js
+++ src/components/Gallery/style.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 export const Card = styled.article`
-	width: calc(25% - 20px);
-	padding-right: 20px;
+	width: calc(17% - 20px);
 
 	div img {
 		max-width: 100%;
+		border-style: none;
+		border: 0;
 	}
 `
 
 export const GalleryWrapper = styled.section`
 	display: flex;
+	justify-content: space-evenly;
 	padding: 0 55px;
 `
 
